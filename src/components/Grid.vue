f2630474d7e4af03f590b2f25643f28acf1f03f3
TylerRice27 <Tylerrayrice2@gmail.com>
2023-06-27 16:13:48 -0600

    Grid System with cols, row and justify

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -3,9 +3,15 @@
     <h3>Grid</h3>
     <v-card class="mt-5 mb-8">
       <v-card-text>
+        <!-- v-container accepts props -->
         <v-container>
+          <!-- no-gutters take away space -->
+          <!-- dense will make the space around your cols and rows smaller -->
           <v-row>
-            <v-col cols="12">
+            <!-- cols for the amount of columns -->
+            <!-- Can do offset as well -->
+            <!-- NOTE col-6 at md and up -->
+            <v-col cols="12" md="6">
               <v-card
                 class="pa-2"
                 outlined
@@ -15,16 +21,8 @@
               </v-card>
             </v-col>
           </v-row>
-          <v-row>
-            <v-col cols="4">
-              <v-card
-                class="pa-2"
-                outlined
-                tile
-              >
-                Column
-              </v-card>
-            </v-col>
+          <!-- NOTE justify content like this/ Look into align items syntax -->
+          <v-row justify="space-between">
             <v-col cols="4">
               <v-card
                 class="pa-2"
